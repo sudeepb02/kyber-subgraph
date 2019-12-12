8e62a74e77510206638d7d6c587864d7c772383f
sudeepb02 <sudeepbiswas02@gmail.com>
2019-12-12 19:59:15 +0000

    Add KyberNetwork Contract and refactor code

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,82 +1,34 @@
 import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  Contract,
-  ExecuteTrade,
-  KyberNetworkSet,
-  TokenWithdraw,
-  EtherWithdraw,
-  TransferAdminPending,
-  AdminClaimed,
-  AlerterAdded,
-  OperatorAdded
-} from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { KyberProxyContract, KyberNetworkSet } from "../generated/KyberProxyContract/KyberProxyContract"
+import { KyberContract, AddReserveToNetwork } from "../generated/KyberContract/KyberContract";
+import { ExampleEntity,
+         Kyber,
+         Reserve
+         } from "../generated/schema"
 
-export function handleExecuteTrade(event: ExecuteTrade): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleKyberNetworkSet(event: KyberNetworkSet): void {
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.trader = event.params.trader
-  entity.src = event.params.src
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.enabled(...)
-  // - contract.pendingAdmin(...)
-  // - contract.getOperators(...)
-  // - contract.swapTokenToEther(...)
-  // - contract.maxGasPrice(...)
-  // - contract.kyberNetworkContract(...)
-  // - contract.getUserCapInWei(...)
-  // - contract.swapTokenToToken(...)
-  // - contract.getAlerters(...)
-  // - contract.getExpectedRate(...)
-  // - contract.getUserCapInTokenWei(...)
-  // - contract.info(...)
-  // - contract.getBalance(...)
-  // - contract.admin(...)
 }
 
-export function handleKyberNetworkSet(event: KyberNetworkSet): void {}
+export function handleAddReserveToNetwork(event: AddReserveToNetwork): void {
+  
+  //Load global Kyber entity
+  let kyber = Kyber.load("1")
+  if (kyber == null) {
+    kyber = new Kyber("1")
+    kyber.reservesCount = 0
+  }
+  let add = event.params.add
+  //If a new reserve is being added
+  if (add) {
+    let reserve = new Reserve(event.params.reserve.toHexString())
+    reserve.save()
+    kyber.reservesCount = kyber.reservesCount + 1
+  } else {
+    kyber.reservesCount = kyber.reservesCount - 1
+  }
 
-export function handleTokenWithdraw(event: TokenWithdraw): void {}
+  kyber.save()
 
-export function handleEtherWithdraw(event: EtherWithdraw): void {}
+}         
 
-export function handleTransferAdminPending(event: TransferAdminPending): void {}
-
-export function handleAdminClaimed(event: AdminClaimed): void {}
-
-export function handleAlerterAdded(event: AlerterAdded): void {}
-
-export function handleOperatorAdded(event: OperatorAdded): void {}
